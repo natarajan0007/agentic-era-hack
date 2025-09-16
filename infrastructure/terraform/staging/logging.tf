0530d5bf0eb8824e76deefadd681eaf26f1f24bd
natarajan.muthu <natarajan.muthu@wipro.com>
2025-09-16 23:29:19 +0100

    Add loggin

--- infrastructure/terraform/staging/logging.tf
+++ infrastructure/terraform/staging/logging.tf
@@ -11,3 +11,11 @@ resource "google_storage_bucket_iam_member" "cloudbuild_logs_writer" {
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.cicd_runner_sa.email}"
 }
+
+data "google_project" "project" {}
+
+resource "google_storage_bucket_iam_member" "cloudbuild_sa_logs_writer" {
+  bucket = google_storage_bucket.cloudbuild_logs.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
